bffff520b6abfd4cbf098aca98b8122e26af61bb
humhaisk <skiku2002@gmail.com>
2025-12-23 00:18:20 +0530

    database connected

--- mcl_project/bidBackend/settings.py
+++ mcl_project/bidBackend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # import channels_redis
 
@@ -92,10 +93,10 @@ ASGI_APPLICATION = 'bidBackend.asgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600
+    )
 }
 
 
